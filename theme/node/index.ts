2145d338a9d9943ace6f5a999fdcb0cabad4aaa1
sanshiliuxiao <sanshiliuxiao@foxmail.com>
2023-04-16 11:28:39 +0800

    初步的默认主题样式

--- theme/node/index.ts
+++ theme/node/index.ts
@@ -4,7 +4,67 @@ import type { ThemeConfig } from '../types'
  * Default Config
  */
 export const defaultThemeConfig: ThemeConfig = {
-  colors: {
-    primary: '#0078E7',
+  title: '椎咲良田',
+  subtitle: '快走吧 趁风停止之前',
+  author: '椎咲良田',
+  description: '世界并不美丽，却也因此而美丽',
+  avatar: 'https://cdn.jsdelivr.net/gh/sanshiliuxiao/blog-static/avatar.jpg',
+  favicon: 'https://cdn.jsdelivr.net/gh/sanshiliuxiao/blog-static/favicon.jpg',
+  domain: 'zmxlt.top',
+  pcImage: 'https://cdn.jsdelivr.net/gh/sanshiliuxiao/blog-static/pc.png',
+  mobileImage: 'https://cdn.jsdelivr.net/gh/sanshiliuxiao/blog-static/mobile.png',
+  postImage: 'https://cdn.jsdelivr.net/gh/sanshiliuxiao/blog-static/postDefaultImage .png',
+  loadingImage: 'https://cdn.jsdelivr.net/gh/sanshiliuxiao/blog-static/loading.gif',
+  menu: [
+    {
+      text: '首页',
+      link: '/',
+      quote: '',
+      icon: 'icon-index',
+    },
+    {
+      text: '归档',
+      link: '/archives/',
+      quote: '華枝春滿 天心月圓',
+      icon: 'icon-archives',
+    }, {
+      text: '分类',
+      link: '/categories/',
+      quote: '桜華月想 暮色蒼然',
+      icon: 'icon-categories',
+    },
+    {
+      text: '标签',
+      link: '/tags/',
+      quote: '灯笼流丽，百鬼夜行',
+      icon: 'icon-tags',
+    }, {
+      text: '书单',
+      link: '/books/',
+      quote: '追想風景 彼岸帰航',
+      icon: 'icon-books',
+    },
+    {
+      text: '灵感',
+      link: '/inspirations/',
+      quote: '詠奏妖華，明鏡止水',
+      icon: 'icon-inspirations',
+    }, {
+      text: '友链',
+      link: '/friends/',
+      quote: '青青子衿，悠悠我心',
+      icon: 'icon-friends',
+    },
+    {
+      text: '关于',
+      link: '/about/',
+      quote: '蝉鸣如雨，花宵道中',
+      icon: 'icon-about',
+    },
+  ],
+  footer: {
+    since: 2022,
+    title: '椎咲良田',
+    subtitle: '快走吧 趁风停止之前',
   },
 }
--- theme/types/index.d.ts
+++ theme/types/index.d.ts
@@ -1,5 +1,51 @@
 /**
  * Theme Config
  */
-export interface ThemeConfig {}
+export interface ThemeConfig {
+  title: string
+  subtitle: string
+  author: string
+  description: string
+  avatar: string
+  favicon: string
+  domain: string
+  /**
+   * Header 栏的子页面
+   */
+  menu: Partial<Menu[]>
+  /**
+   * 背景图片(电脑页面/手机页面) 默认文章封面, 加载图片
+   */
+  pcImage: string
+  mobileImage: string
+  postImage: string
+  loadingImage: string
+  footer: Partial<{
+    since: number
+    /**
+     * Powered by valaxy & valaxy-theme-${name}, default is yun
+     */
+    powered: boolean
+    title: string
+    subtitle: string
+    /**
+     * Chinese Users | 中国用户
+     * 备案 ICP
+     * 国内用户需要在网站页脚展示备案 ICP 号
+     * https://beian.miit.gov.cn/
+     */
+    beian: {
+    enable: boolean
+    icp: string
+    }
+  }>
+}
+
+export interface Menu {
+  text: string
+  link: string
+  quote: string
+  icon: string
+}
+
 export type ThemeUserConfig = Partial<ThemeConfig>
